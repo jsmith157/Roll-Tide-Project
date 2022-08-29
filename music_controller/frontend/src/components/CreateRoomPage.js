fc564275d758808eba0a35b8ee36734d839a6fc3
jsmith157 <70680292+jsmith157@users.noreply.github.com>
2022-08-28 19:21:53 -0500

    added buttons and functionality

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography  from "@material-ui/core/Typography";
+import TextField  from "@material-ui/core/TextField";
+import FormHelperText  from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
     constructor(props) {
@@ -6,6 +16,39 @@ export default class CreateRoomPage extends Component {
     }
 
     render() {
-        return <p>Your leading the charge of this room</p>;
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} allign="center">
+                   <Typography component="h4" variant="h4">
+                        Create A Room
+                    </Typography>   
+                </Grid>
+                <Grid item xs={12} allign="center">
+                    <FormControl component="fieldset">
+                        <FormHelperText >
+                            <div allign="center">
+                                Guest Control of Playback
+                            </div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue="true">
+                            <FormControlLabel 
+                                value="true" 
+                                control={<Radio color="primary" />}
+                                label="Play/Pause"
+                                labelPlacement="bottom"  
+                            />
+                            <FormControlLabel 
+                                value="false" 
+                                control={<Radio color="secondary" />}
+                                label="No Control"
+                                labelPlacement="bottom"  
+                            />
+                            
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>           
+            </Grid>
+            
+        );
     }
 }
